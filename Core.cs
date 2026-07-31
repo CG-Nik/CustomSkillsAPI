dbb0ec7ba935ea16a3c74a7d0a6de0be20b342b2
Nikolai Hovis <nikolai.hovis@gmail.com>
2026-07-31 14:28:27 -0500

    Add project files.

---
+++ Core.cs
@@ -0,0 +1,21 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(CustomSkillsAPI.Core), "CustomSkillsAPI", "1.0.0", "CGNik", null)]
+[assembly: MelonGame("Alta", "A Township Tale")]
+
+namespace CustomSkillsAPI
+{
+    public class Core : MelonMod
+    {
+        public static event Action SetUpProgressionSlots = () => { };
+        public static event Action AddProgressionSlots = () => { };
+        public static event Action AddInherits = () => { };
+
+        public override void OnLateInitializeMelon()
+        {
+            SetUpProgressionSlots.Invoke();
+            AddProgressionSlots.Invoke();
+            AddInherits.Invoke();
+        }
+    }
+}
