41b5f333c2f9e3a9784199231aa82bee8a354fdd
Nikolai Hovis <nikolai.hovis@gmail.com>
2026-08-08 15:12:09 -0500

    actually an API now

--- Core.cs
+++ Core.cs
@@ -1,4 +1,6 @@
-﻿using MelonLoader;
+﻿using Alta;
+using MelonLoader;
+using System.Reflection;
 
 [assembly: MelonInfo(typeof(CustomSkillsAPI.Core), "CustomSkillsAPI", "1.0.0", "CGNik", null)]
 [assembly: MelonGame("Alta", "A Township Tale")]
@@ -11,6 +13,8 @@ namespace CustomSkillsAPI
         public static event Action AddProgressionSlots = () => { };
         public static event Action AddInherits = () => { };
 
+        public static List<ProgressionSlot> progressionSlots = [];
+
         public override void OnInitializeMelon()
         {
             LoggerInstance.Msg("Initialized.");
@@ -19,8 +23,27 @@ namespace CustomSkillsAPI
         public override void OnLateInitializeMelon()
         {
             SetUpProgressionSlots.Invoke();
-            AddProgressionSlots.Invoke();
-            AddInherits.Invoke();
+
+            foreach (ProgressionSlot progressionSlot in progressionSlots)
+            {
+                ProfessionSkillTree professionSkillTree = ProfessionSkillTree.GetProfessionTree(progressionSlot.Path);
+
+                Dictionary<uint, ProgressionSlot> slotMap = (Dictionary<uint, ProgressionSlot>)professionSkillTree.GetType().GetField("slotMap", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(professionSkillTree);
+                slotMap[progressionSlot.Hash] = progressionSlot;
+                professionSkillTree.Slots.Add(progressionSlot);
+            }
+
+            foreach (ProgressionSlot progressionSlot in progressionSlots)
+            {
+                progressionSlot.AddInherit(ProfessionSkillTree.GetProfessionTree(progressionSlot.Path));
+            }
+        }
+
+        public static void RegisterProfessionSkill(ProfessionSkill professionSkill)
+        {
+            ProfessionSkill.CheckItems();
+            Dictionary<uint, ProfessionSkill> items = (Dictionary<uint, ProfessionSkill>)typeof(HashedGeneralValue<ProfessionSkill>).GetField("items", BindingFlags.Static | BindingFlags.NonPublic).GetValue(null);
+            items.Add(professionSkill.Hash, professionSkill);
         }
     }
 }
